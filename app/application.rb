4287c0f832f60ac478faa0c00e3b335550f1b4eb
Jessica Gladstone <jcg321@comcast.net>
2019-02-03 23:44:42 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,10 +2,12 @@ class Application
 def call(env)
     resp = Rack::Response.new
     
-    time = Time.now
+    if Time.now > 12
+      resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning!"
+    end
     
-    
-    resp.write "Hello, World"
     resp.finish
   end
  
